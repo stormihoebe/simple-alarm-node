dae74ac4c7e649d6eefb6322507c09b259d2ba72
Stormi Hoebelhienrich <stormihoebe@gmail.com>
2017-04-18 10:43:04 -0700

    add gulp and bower and used js Moment to check current time against alarm set time

--- js/alarm.js
+++ js/alarm.js
@@ -1,15 +1,18 @@
 function Alarm(hour, minute) {
   this.hour = hour;
   this.minute = minute;
-};
+}
 
 Alarm.prototype.alarmSound = function() {
-  var currentTimeH = parseInt(moment().format('HH'));
-  var currentTimeM = parseInt(moment().format('mm'));
-  if ((hour === currentTimeH) && (minute === currentTimeM)) {
-    $('#alarmOutput').show();
-    $('#timeForm').hide();
+  var alarmSounding = false;
+  var currentTimeH = parseInt(moment().format('H'));
+  var currentTimeM = parseInt(moment().format('m'));
+  if ((this.hour === currentTimeH) && (this.minute === currentTimeM)) {
+    alarmSounding = true;
   } else {
-    $('#alarmOutput').hide();
+    alarmSounding = false;
   }
+  return alarmSounding;
 };
+
+exports.alarmModule = Alarm;
