7f6f0724e23826589b25e53906e460fb96193e60
Stormi Hoebelhienrich <stormihoebe@gmail.com>
2017-04-18 10:55:48 -0700

    add bower

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,57 @@
-!function t(r,e,n){function o(u,a){if(!e[u]){if(!r[u]){var m="function"==typeof require&&require;if(!a&&m)return m(u,!0);if(i)return i(u,!0);var l=new Error("Cannot find module '"+u+"'");throw l.code="MODULE_NOT_FOUND",l}var s=e[u]={exports:{}};r[u][0].call(s.exports,function(t){var e=r[u][1][t];return o(e||t)},s,s.exports,t,r,e,n)}return e[u].exports}for(var i="function"==typeof require&&require,u=0;u<n.length;u++)o(n[u]);return o}({1:[function(t,r,e){function n(t,r){this.hour=t,this.minute=r}n.prototype.alarmSound=function(){var t=parseInt(moment().format("H")),r=parseInt(moment().format("m"));return this.hour===t&&this.minute===r},e.alarmModule=n},{}],2:[function(t,r,e){var n=t("./../js/alarm.js").alarmModule;$(function(){$("#alarmOutput").hide(),$("#timeForm").submit(function(t){function r(){!0===u.alarmSound()?($("#alarmOutput").show(),$("#timeForm").hide(),console.log("TIME!!!")):($("#alarmOutput").hide(),console.log("I am checking, it isn't time yet"))}t.preventDefault();var e=$("#inputTime").val(),o=parseInt(e.slice(0,2)),i=parseInt(e.slice(3,5)),u=new n(o,i);setInterval(r,1e3)})})},{"./../js/alarm.js":1}]},{},[2]);
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+function Alarm(hour, minute) {
+  this.hour = hour;
+  this.minute = minute;
+}
+
+Alarm.prototype.alarmSound = function() {
+  var alarmSounding = false;
+  var currentTimeH = parseInt(moment().format('H'));
+  var currentTimeM = parseInt(moment().format('m'));
+  if ((this.hour === currentTimeH) && (this.minute === currentTimeM)) {
+    alarmSounding = true;
+  } else {
+    alarmSounding = false;
+  }
+  return alarmSounding;
+};
+
+exports.alarmModule = Alarm;
+
+},{}],2:[function(require,module,exports){
+var Alarm = require('./../js/alarm.js').alarmModule;
+
+
+$(function(){
+  $('#alarmOutput').hide();
+
+  $('#timeForm').submit(function(event) {
+      event.preventDefault();
+      var inputTime = $('#inputTime').val();
+      var inputH = parseInt(inputTime.slice(0,2));
+      var inputM = parseInt(inputTime.slice(3,5));
+      // console.log(inputTime);
+      // console.log(inputH);
+      // console.log(inputM);
+      var newAlarm = new Alarm(inputH, inputM);
+      function checkAlarm() {
+        var alarmSounding = newAlarm.alarmSound();
+        if (alarmSounding === true) {
+          $('#alarmOutput').show();
+          $('#timeForm').hide();
+          console.log("TIME!!!");
+
+        } else {
+          $('#alarmOutput').hide();
+          console.log("I am checking, it isn't time yet");
+        }
+      }
+      var interval = setInterval(checkAlarm, 1000);
+
+    });
+
+
+
+});
+
+},{"./../js/alarm.js":1}]},{},[2]);
